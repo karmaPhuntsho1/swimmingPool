349bb79c7bb1b511570a06e244b18837fd724e1a
karmaPhuntsho1 <phuntshokarma75@gmail.com>
2025-08-04 13:31:19 +0600

    Fourth Commit

--- config/db.js
+++ config/db.js
@@ -9,7 +9,9 @@ const db = pgp({
   database: process.env.DB_NAME,
   user: process.env.DB_USER, 
   password: process.env.DB_PASS, 
-  ssl: false
+  ssl: {
+    rejectUnauthorized: false
+  } 
 });
 
 module.exports = db;
